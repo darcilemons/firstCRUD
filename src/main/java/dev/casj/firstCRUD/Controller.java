eacd2011c6c0287c97b3aa2aead58b1fad03bc15
carlos <darcilemons@gmail.com>
2025-07-15 14:14:10 -0300

    Criado a controller

---
+++ src/main/java/dev/casj/firstCRUD/Controller.java
@@ -0,0 +1,16 @@
+package dev.casj.firstCRUD;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Controlador para uma API Rest
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas") //pega as informações
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+}
