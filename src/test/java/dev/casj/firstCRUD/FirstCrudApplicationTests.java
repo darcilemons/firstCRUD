8987f3373ea8a8f7f5193478a25b26d830814631
carlos <darcilemons@gmail.com>
2025-07-15 10:20:56 -0300

    initial commit

---
+++ src/test/java/dev/casj/firstCRUD/FirstCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.casj.firstCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
